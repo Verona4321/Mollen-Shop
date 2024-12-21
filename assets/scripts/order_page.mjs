74c727f6051e7f1260ed28c9326b1c1b5adb21d4
Yana <isaychevayana@gmail.com>
2024-12-21 03:57:34 +0300

    1

--- assets/scripts/order_page.mjs
+++ assets/scripts/order_page.mjs
@@ -1,50 +1,53 @@
 // JSON для наглядности:
 
-// const json = [
-//     {
-//         "type": "pillowcase",
-//         "name": "Наволочка Журавль",
-//         "icon": "prints/print_11.jpg",
-//         "image": "pillowcases/zhurrr.webp",
-//         "material": "Шелк и сатин",
-//         "size": "50x70 см",
-//         "description": "Наволочка из премиальных сортов шелка или сатина. Коллекция посвящена животным и растениям Красной Книги России и призывает напомнить о бережном отношении к природе. Журавль Красавка является одной из самых красивых степных птиц. Эти маленькие птицы с удовольствием селятся на территории России, заводят пару, верность которой сохраняют всю жизнь.",
-//         "price": 3200,
-//         "quantity": 1
-//     },
-//     {
-//         "type": "pillowcase",
-//         "name": "Наволочка Таинственный лес",
-//         "icon": "prints/forest_1.webp",
-//         "image": "pillowcases/zhurrr.webp",
-//         "material": "Шелк и сатин",
-//         "size": "50x70 см",
-//         "description": "Орнамент Таинственный лес, олицетворяющий собой новые тренды в дизайне интерьера, опирается на классический русский узор и отдает дань уважения славянской традиции. Лес - священное место древних славян, он полон тайн и населен могущественными загадочными существами. Гамма благородных природных цветов Mollen, представленная на рисунке, создана лучшими колористами России.",
-//         "price": 3200,
-//         "quantity": 2
-//     },
-//     {
-//         "type": "pillowcase",
-//         "name": "Наволочка Алконост",
-//         "icon": "prints/alkonost.webp",
-//         "image": "pillowcases/zhurrr.webp",
-//         "material": "Шелк и сатин",
-//         "size": "50x70 см",
-//         "description": "Волшебная птица Алконост, изображенная на ткани, согласно древнерусским преданиям, прилетает на землю из рая и приносит радость и умиротворение. Чудесное создание порадует вас своей красотой и поможет забыть тревоги и заботы.",
-//         "price": 3200,
-//         "quantity": 3
-//     }
-// ]
-// // Товары в корзине (локал стореж) для примера:
+const json = [
+    {
+        "type": "pillowcase",
+        "name": "Наволочка Журавль",
+        "icon": "prints/print_11.jpg",
+        "image": "pillowcases/zhurrr.webp",
+        "material": "Шелк и сатин",
+        "size": "50x70 см",
+        "description": "Наволочка из премиальных сортов шелка или сатина. Коллекция посвящена животным и растениям Красной Книги России и призывает напомнить о бережном отношении к природе. Журавль Красавка является одной из самых красивых степных птиц. Эти маленькие птицы с удовольствием селятся на территории России, заводят пару, верность которой сохраняют всю жизнь.",
+        "price": 3200,
+        "quantity": 1
+    },
+    {
+        "type": "pillowcase",
+        "name": "Наволочка Таинственный лес",
+        "icon": "prints/forest_1.webp",
+        "image": "pillowcases/zhurrr.webp",
+        "material": "Шелк и сатин",
+        "size": "50x70 см",
+        "description": "Орнамент Таинственный лес, олицетворяющий собой новые тренды в дизайне интерьера, опирается на классический русский узор и отдает дань уважения славянской традиции. Лес - священное место древних славян, он полон тайн и населен могущественными загадочными существами. Гамма благородных природных цветов Mollen, представленная на рисунке, создана лучшими колористами России.",
+        "price": 3200,
+        "quantity": 2
+    },
+    {
+        "type": "pillowcase",
+        "name": "Наволочка Алконост",
+        "icon": "prints/alkonost.webp",
+        "image": "pillowcases/zhurrr.webp",
+        "material": "Шелк и сатин",
+        "size": "50x70 см",
+        "description": "Волшебная птица Алконост, изображенная на ткани, согласно древнерусским преданиям, прилетает на землю из рая и приносит радость и умиротворение. Чудесное создание порадует вас своей красотой и поможет забыть тревоги и заботы.",
+        "price": 3200,
+        "quantity": 3
+    }
+]
+// Товары в корзине (локал стореж) для примера:
 
-// const json2 = JSON.stringify(json)
-// window.localStorage.setItem('cart', json2);
-// window.localStorage.setItem('wrapping', true);
+const json2 = JSON.stringify(json)
+window.localStorage.setItem('cart', json2);
+window.localStorage.setItem('wrapping', true);
+
 
 
 // Консты:
 
     const productList = JSON.parse(localStorage.getItem('cart'));
+
+
     const divContainer = document.querySelector('.order-info__cart-products-list');
     const totalQuantity = document.querySelector('.totals__quantity-result')
     const totalPrice = document.querySelector('.totals__discount-result')
@@ -57,40 +60,62 @@
     const body = document.querySelector('.body');
     const checkValidate = document.querySelectorAll('.checkValidate')
     const agreeTerms = document.getElementById('agreeTerms');
-    const finalHTML = `<main class="main_final">
-                            <section class="section_final">
-                                <div>
-                                    <nav class="menu">
-                                        <ul class="menu-list">
-                                            <li class="menu-item"><a class="menu-link" href="#">Главная</a></li>
-                                            <span>/</span>
-                                            <li class="menu-item"><a class="menu-link" href="#">Корзина</a></li>
-                                        </ul>
-                                    </nav>
-                                    <h1 class="final_message-title"></h1>
-                                </div>
-                                <div>
-                                    <p class="final_message"></p>
+    const form = document.forms[0];
+    const formElements = form.elements;
+    const button = document.querySelector('.totals__btn');
+    // const finalHTML = `<main class="main_final">
+    //                         <section class="section_final">
+    //                             <div>
+    //                                 <nav class="menu">
+    //                                     <ul class="menu-list">
+    //                                         <li class="menu-item"><a class="menu-link" href="#">Главная</a></li>
+    //                                         <span>/</span>
+    //                                         <li class="menu-item"><a class="menu-link" href="#">Корзина</a></li>
+    //                                     </ul>
+    //                                 </nav>
+    //                                 <h1 class="final_message-title"></h1>
+    //                             </div>
+    //                             <div>
+    //                                 <p class="final_message"></p>
                                     
-                                    <button class="final_btn" onclick="window.location.href='#';"></button>
-                                </div>
-                            </section>
-                            <aside class="aside_final">
-                            </aside>
-                        </main>`
-    const finalScript = `<script src='assets/scripts/order_sucsess.js'></script>`;
-
+    //                                 <button class="final_btn"><a class="final_link"></a></button>
+    //                             </div>
+    //                         </section>
+    //                         <aside class="aside_final">
+    //                         </aside>
+    //                     </main>`
+    // const finalScript = `<script src='assets/scripts/order_success.js'></script>`;
+    // let productList = [];
 
     document.querySelector(".totals__btn").disabled = true;
 
+// Рендер корзины:   
+//     const renderProductList = fetch('http://localhost:3000/cart', {
+//         method: 'GET'})
+//         .then((res) => {
+//             return res.json();
+//         })
+//         .then((data) => {
+//             productList = data;
 
-// Рендер корзины из локал стореж:   
+//             for (let item of productList) {
+//                 renderProductCart (item);
+
+
+
+//         }})
+    
+//         .catch((err) => {
+//             body.innerHTML = finalHTML;
+//             document.querySelector('.final_message-title').textContent = "Произошла ошибка";
+//             document.querySelector('.final_link').textContent = "Вернуться на главную";
+// });
+
 
 function renderProductCart (item) {
     const div = document.createElement("div")
     div.classList.add("cart-products-list-item", 'card');
     div.innerHTML =
-            // `<button type="button" class="delete-item__btn"><img src="assets/images/delete.svg"></img></button>
             `<div class="product-img"><img src="assets/images/${item.image}"></img></div>
             <a class="product-name" href='order_error.html'>${item.name}</a>
             <div class="product-quantity">
@@ -98,16 +123,19 @@ function renderProductCart (item) {
             </div>
             <div class="product-price">${item.price}</div>`;
     divContainer.append(div);
-}
+};
+
 
 productList.forEach(product => {
     renderProductCart (product)
-    })
+    });
+
+
 
 
 //Подсчет и рендер итогов:
 
-export const countQuantity = (productList) => {
+const countQuantity = () => {
     let resultQuantity = 0;
     productList.forEach(product => {
         resultQuantity += Number(product.quantity);
@@ -115,8 +143,10 @@ export const countQuantity = (productList) => {
     return resultQuantity;
     };
 
-function finalCount() {
-    countTotals = () => {
+export default countQuantity;
+
+
+const countTotals = () => {
     let resultPrice = 0;
     productList.forEach(product => {
         resultPrice += (Number(product.price)* Number(product.quantity));
@@ -129,12 +159,14 @@ function finalCount() {
     
     totalQuantity.textContent = countQuantity();
 
-if (localStorage.getItem('wrapping')){
-    wrapping.textContent = '500'}
-    else {wrapping.textContent = 'Нет'};
+    if (localStorage.getItem('wrapping')){
+        wrapping.textContent = '500'}
+        else {wrapping.textContent = 'Нет'};
 
-totalOrderPrice.textContent = countTotals() + Number(wrapping.textContent) + 500;
-}
+    totalOrderPrice.textContent = countTotals() + Number(wrapping.textContent) + 500;
+
+
+
 
 deliveryBtn.forEach(function (btn) {
     btn.addEventListener('click', function() {
@@ -154,11 +186,7 @@ deliveryBtn.forEach(function (btn) {
 
 //   Валидация формы:
 
-const form = document.forms[0];
-const formElements = form.elements;
-const button = document.querySelector('.totals__btn');
-
-var cleave = new Cleave('#tel', {
+let cleave = new Cleave('#tel', {
     phone: true,
     phoneRegionCode: 'RU'
 });
@@ -181,22 +209,22 @@ checkValidate.forEach(function (el) {
 
 // Функция сбора данных формы и корзины:
 
-let data = {};
+let cartData = {};
+
 const createData = () => {
 
     $.each($('.form').serializeArray(), function(i, field) {
-        data[field.name] = field.value;
+        cartData[field.name] = field.value;
     });
     
-    data.products = localStorage.getItem('cart');
-    data.wrapping = localStorage.getItem('wrapping');
-return data;
+    cartData.products = localStorage.getItem('cart');
+    cartData.wrapping = localStorage.getItem('wrapping');
+return cartData;
 };
 
 
 
 //Валидация формы:
-
 
 $(document).ready(function() {
     
@@ -243,31 +271,38 @@ form.addEventListener ("submit", async(e) => {
 
     createData();
 
+
     const response = await fetch('http://localhost:3000/orders', {
         method: 'POST',
         body: JSON.stringify(createData())})
 
                 .then(res => {
                     if (res.ok) {
-                        body.innerHTML = finalHTML + finalScript;
-                        document.querySelector('.final_message-title').textContent = "Заказ оформлен";
-                        document.querySelector('.final_link').textContent = "На главную";
+                        // body.innerHTML = finalHTML + finalScript;
+                        // document.querySelector('.final_message-title').textContent = "Заказ оформлен";
+                        // document.querySelector('.final_link').textContent = "На главную";
                         localStorage.removeItem('cart', 'wrapping');
-                        // window.location = 'order_success.html';
+                        window.location = 'order_success.html';
                         form.reset;
+                //      const refresh = fetch('http://localhost:3000/cart', {
+                //         method: 'DELETE',
+                //     })
+
                     }
                     else { 
-                        body.innerHTML = finalHTML;
-                        document.querySelector('.final_message-title').textContent = "Произошла ошибка";
-                        document.querySelector('.final_link').textContent = "Вернуться в корзину";
-                        // window.location = 'order_error.html'
+                        // body.innerHTML = finalHTML;
+                        // document.querySelector('.final_message-title').textContent = "Произошла ошибка";
+                        // document.querySelector('.final_link').textContent = "Вернуться в корзину"
+                        window.location = 'order_error.html'
 
                     }
                 })
                 .catch((err) => {
-                    body.innerHTML = finalHTML;
-                    document.querySelector('.final_message-title').textContent = "Произошла ошибка";
-                    document.querySelector('.final_link').textContent = "Вернуться в корзину";
-                    // window.location = 'order_error.html'
+                    // body.innerHTML = finalHTML;
+                    // document.querySelector('.final_message-title').textContent = "Произошла ошибка";
+                    // document.querySelector('.final_link').textContent = "Вернуться в корзину";
+                    window.location = 'order_error.html'
                 })
+
+
 });
